6f6d90333d431bc9fe15fa50c56dc801d2c198b5
YOUR NAME GOES HERE <YOUR EMAIL GOES HERE @ your domain>
2019-07-22 11:16:10 +0530

    after resolving conflicts

--- Sample_program.py
+++ Sample_program.py
@@ -1,14 +1,14 @@
 Dict1 = {1: 2000, 2:3000}
 print(Dict1)
-
+# My comment goes here
 for x in Dict1.values():
     print(x)
 
 #This is the conditional check here
 # This is the second comment
-# This is third comment
-
-if not Dict1:
+#This is the third comment
+#Hi
+#if not Dict1:
     print("Dict is empty")
 else:
     print("Dict is not empty")
